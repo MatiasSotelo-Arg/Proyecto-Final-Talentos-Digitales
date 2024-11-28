47840efb53f60e102e38783d162648699eadb16a
WarnickAJA <javierq666@gmail.com>
2024-11-28 00:12:56 -0300

    feat: login, logout y profile con Auth0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import ItemList from './components/ItemList/ItemList'
-import items from './data/data.json'
-import ItemDetail from './components/ItemDetail/ItemDetail'
+import { useState } from "react";
+import "./App.css";
+import ItemList from "./components/ItemList/ItemList";
+import items from "./data/data.json";
+import ItemDetail from "./components/ItemDetail/ItemDetail";
+
+import Login from "./components/Auth0/Login";
+import Logout from "./components/Auth0/Logout";
+import Profile from "./components/Auth0/Profile";
 
 function App() {
-  
   const item = items[0];
-  console.log(items)
+  console.log(items);
   return (
     <>
-     <ItemList items={items}/>
-     <ItemDetail item={item}/>
+    <Login/>
+    <Logout/>
+    <Profile/>
+      <ItemList items={items} />
+      <ItemDetail item={item} />
+      
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
