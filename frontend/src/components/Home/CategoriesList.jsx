2e3418f98802b9fc9b9a7a250d826c9bab0a77ea
WarnickAJA <javierq666@gmail.com>
2024-12-09 18:53:08 -0300

    fix: useEffect

--- frontend/src/components/Home/CategoriesList.jsx
+++ frontend/src/components/Home/CategoriesList.jsx
@@ -1,43 +1,35 @@
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import useFetch from "../../hooks/useFetch";
-import { useSelector, useDispatch } from "react-redux";
 import { getCourses } from "../../redux/coursesSlice";
 
 const CategoriesList = () => {
-  // const [categories, setCategories] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //     const getCategories = async() => {
-  //         const data = await fetchCourse();
-  //         setCategories(data);
-  //         setLoading(false)
-  //     }
-
-  //     getCategories();
-  // }, []);
-
   const { data, loading, error } = useFetch(
     import.meta.env.VITE_API_URL + "api/courses"
   );
-
-  if (error) return <p>Error: {error.message}</p>;
-
-  const courses = useSelector((state) => state.courses);
-
   const dispatch = useDispatch();
 
-  dispatch(getCourses(data));
-  console.log("Redux: ", courses);
-  if (loading) return <p>Recibiendo las categorias...</p>;
+  useEffect(() => {
+    if (data) {
+      dispatch(getCourses(data));
+    }
+  }, [data, dispatch]);
+
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const uniqueCategories = [...new Set(data.map((course) => course.category))];
 
   return (
-    <>
-      {courses &&
-        courses.map((course) => (
-          <div key={course?._id}>{course?.category}</div>
-        ))}
-    </>
+    <ul>
+      {/* {data.map((course) => (
+        <li key={course._id}>{course.category}</li>
+      ))} */}
+
+      {uniqueCategories.map((category) => (
+        <li key={category}>{category}</li>
+      ))}
+    </ul>
   );
 };
 
