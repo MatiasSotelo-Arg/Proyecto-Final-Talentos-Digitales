b2dad472a2fd0202cb25a584e519603a97ffccd4
Matias Sotelo <matysotelo07@gmail.com>
2024-12-11 23:59:44 -0300

    act files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import Home from "./components/Home/Home";
+import Home from "./components/FileHome/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Pruebas from "./components/pruebas";
 
-import CoursesContainer from "./components/CoursesContainer/CoursesContainer";
-import ItemDetail from "./components/CourseDetail/ItemDetail";
+import CoursesContainer from "./components/CoursesContainerList/CoursesContainer";
+import CourseDetail from "./components/CourseDetail/CourseDetail";
 import DataLoader from "./components/DataLoader/DataLoader";
 import CartContainer from "./components/CartContainer/CartContainer";
-import UserCourses from "./components/userCourses/UserCourses";
+import UserCourses from "./components/UserCourses/UserCourses";
 import EditCourse from "./components/CoursesCRUD/EditCourse";
 import FooterContainer from "./components/Footer/FooterContainer/FooterContainer";
 import LoadUserToDB from "./components/Auth0/LoadUserToDB";
@@ -27,9 +27,9 @@ function App() {
           <Route path="/pruebas" element={<Pruebas />} />
           <Route path="/carrito" element={<CartContainer />} />
           <Route path="/cursos" element={<CoursesContainer />} />
-          <Route path="/cursos/:cursoId" element={<ItemDetail />} />
-          <Route path="/miscursos" element={<UserCourses />} />
-          <Route path="/modificar" element={<EditCourse />} />
+          <Route path="/cursos/:cursoId" element={<CourseDetail />} />
+          <Route path="/miscursos" element={<UserCourses/>} />
+          <Route path="/modificar" element={<EditCourse/>} />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </div>
--- frontend/src/components/Home/Home.jsx
+++
@@ -1,7 +0,0 @@
-import CategoriesList from "./CategoriesList";
-
-const Home = () => {
-  return <CategoriesList />;
-};
-
-export default Home;
