ae29120cf6ac3f3f15145eab9d75aa8cc3be7322
WarnickAJA <javierq666@gmail.com>
2024-12-18 13:24:32 -0300

    add

--- frontend/src/components/Footer/FooterSocialContainer/FooterSocialContainer.jsx
+++ frontend/src/components/Footer/FooterSocialContainer/FooterSocialContainer.jsx
@@ -1,9 +1,16 @@
-
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 
 const FooterSocialContainer = () => {
   return (
-    <div>FooterSocialContainer</div>
-  )
-}
+    <div>
+      FooterSocialContainer
+      <FaGithub />
+      <FaInstagram />
+      <FaFacebook />
+    </div>
+  );
+};
 
-export default FooterSocialContainer
+export default FooterSocialContainer;
